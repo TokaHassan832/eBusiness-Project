8f931ba24e2253e2e230beb18be187dd3b282e45
toka <tokahassan692@gmail.com>
2023-04-23 06:56:08 +0200

    WIP..

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,12 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
 
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::prefix('blog')->group(function (){
-    Route::get('/',[PostController::class,'index']);
-    Route::get('/posts/{post}',[PostController::class,'show']);
+    Route::get('/',[PostController::class,'index'])->middleware('auth');
+    Route::get('/posts/{post}',[PostController::class,'show'])->middleware('auth');
 });
 
