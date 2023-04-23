b8023e45dfd719375d331c02256f31a4b9755400
toka <tokahassan692@gmail.com>
2023-04-23 03:38:14 +0200

    WIP..

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,10 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-Route::get('/blog', function () {
-    return view('blog');
-});
-Route::get('/blog-details', function () {
-    return view('blog-details');
+
+Route::prefix('blog')->group(function (){
+    Route::get('/',[PostController::class,'index']);
+    Route::get('/posts/{post}',[PostController::class,'show']);
 });
 
