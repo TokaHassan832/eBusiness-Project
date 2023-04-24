b683aad3ffc448347c6b52819c0c1cc08e75add8
toka <tokahassan692@gmail.com>
2023-04-24 05:52:12 +0200

    creating migrations, database seeding & factories

---
+++ database/migrations/2023_04_24_021715_create_team_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('team', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('image')->nullable();
+            $table->string('job_description');
+            $table->string('facebook_link');
+            $table->string('instagram_link');
+            $table->string('twitter_link');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('team');
+    }
+};
